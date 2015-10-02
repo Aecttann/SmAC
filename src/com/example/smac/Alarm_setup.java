46d194eaf16f02bb7ce57b09d59a44dbf5b069a1
Alexander Vasilchuk <aectann101@gmail.com>
2015-10-03 00:13:10 +0300

    lol

---
+++ src/com/example/smac/Alarm_setup.java
@@ -0,0 +1,10 @@
+package com.example.smac;
+import android.app.Activity;
+import android.os.Bundle;
+public class Alarm_setup extends Activity{
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setup_alarm);
+		
+	}
+}
---
+++ src/com/example/smac/Destination_map.java
@@ -0,0 +1,21 @@
+package com.example.smac;
+/*
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+import android.os.Bundle;
+
+public class Destination_map extends MapActivity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.destination);
+		MapView map_for_destination_select = (MapView) findViewById(R.id.map1);
+		map_for_destination_select.setBuiltInZoomControls(true);
+}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}*/
